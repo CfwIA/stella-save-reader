1df530f2718861dd5a70e8abdba20095d2c7133c
CfwIA <23635611+CfwIA@users.noreply.github.com>
2025-10-14 11:13:23 +0900

    Also parses meta file to extract date

--- source.cpp
+++ source.cpp
@@ -14,16 +14,11 @@ int main(int argc, char** argv) {
 	std::string readfilepath = args[1];
 	std::string outfile = args[2];
 
-	auto start = std::chrono::system_clock::now();
 	auto gamestate = parse_savefile(readfilepath);
-	auto timems = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now()-start).count();
-	std::cout << "Parsing took " << timems << "ms\n";
+	std::cout << gamestate.meta_obj["date"].str() << '\n';
 	std::ofstream out(outfile);
 	if (out.is_open()) {
-		auto jsontimestart = std::chrono::system_clock::now();
 		out << gamestate.root_obj.to_json();
-		auto timems2 = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now()-start).count();
-		std::cout << "Printing to json took " << timems2 << "ms\n";
 		out.close();
 	}
 	return 0;
